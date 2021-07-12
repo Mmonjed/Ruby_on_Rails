b96a29ad0e418053ff333c8056516e25c1875227
Mmonjed <mmonjeds@gmail.com>
2021-07-12 17:42:43 -0400

    arreglos.rb

--- arreglos.rb
+++ arreglos.rb
@@ -1,11 +1,35 @@
 a= ["Matz","Guido","Dojo","Choi","John"]
 b= [5,6,9,3,1,2,4,7,8,10]
-puts a[0]
-puts a[1]
+c= ["Dojo", 9]
+#puts a[0]
+#puts a[1]
 
-puts b.class
-puts b.shuffle.join("-")
+#puts a+b
 
-puts a.to_s
-puts b.to_s
-puts 
+#x= a+b
+#x = (a+b)-c
+#puts x.to_s
+
+#puts b.class
+#puts b.shuffle.join("-")
+
+#puts a.to_s
+#puts b.to_s
+
+#a.delete('Choi')
+#puts a
+
+#a.push("mona")
+#puts a
+#a.pop
+
+#puts "el largo de a es: #{a.length}"
+
+#a=%w{"Matz Guido Choi"}
+#puts a
+
+e = %w{gato perro oso}
+puts e.values_at(1,2).join(' y ')
+
+
+
