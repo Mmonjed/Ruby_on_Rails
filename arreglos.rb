85271c5dd2f8daab7cbffbc551daee65b43ced77
Mmonjed <mmonjeds@gmail.com>
2021-07-12 12:43:11 -0400

    ejercicios rb

---
+++ arreglos.rb
@@ -0,0 +1,10 @@
+a= ["Matz","Guido","Dojo","Choi","John"]
+b= [5,6,9,3,1,2,4,7,8,10]
+puts a[0]
+puts a[1]
+
+puts b.class
+puts b.shuffle.join("-")
+
+puts a.to_s
+puts b.to_s
---
+++ tarea1.rb
@@ -0,0 +1,14 @@
+def adivina_numero valor
+    number = 25
+    # tu código aquí
+    if number == valor
+        puts "Lo conseguiste"
+    elsif number > valor
+        puts "El numero es mas pequeño"
+    else
+        puts "El numero es mas grande"
+    end
+end 
+
+adivina_numero(30)
+
